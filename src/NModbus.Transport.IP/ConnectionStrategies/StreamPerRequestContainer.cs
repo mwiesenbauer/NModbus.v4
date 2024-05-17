43cb1d1aa65f5fd3fd6101a2ef7c5a9f62de883a
Michael Wiesenbauer <michael.wiesenbauer@setago.io>
2024-05-17 14:08:14 +0200

    use stream property without private member variable

--- src/NModbus.Transport.IP/ConnectionStrategies/StreamPerRequestContainer.cs
+++ src/NModbus.Transport.IP/ConnectionStrategies/StreamPerRequestContainer.cs
@@ -4,17 +4,15 @@ namespace NModbus.Transport.IP.ConnectionStrategies;
 
 internal class StreamPerRequestContainer : IPerRequestStreamContainer
 {
-    private readonly IModbusStream _stream;
-
     internal StreamPerRequestContainer(IModbusStream stream)
     {
-        _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+        Stream = stream ?? throw new ArgumentNullException(nameof(stream));
     }
 
     public IModbusStream Stream { get; }
 
     public async ValueTask DisposeAsync()
     {
-        await _stream.DisposeAsync();
+        await Stream.DisposeAsync();
     }
 }
